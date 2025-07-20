9523c825be9384482f5db8e6639762c65fc9c5c8
Vlad <underain@gmail.com>
2025-07-20 15:58:49 +0900

    Добавлен лоадер для переходов между страницами

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <main className="h-svh w-full flex items-center justify-center">
+      <div className="loader"></div>
+    </main>
+  );
+};
+
+export default page;
