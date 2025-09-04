42416e45d31bf91dc6401e580e6f01918dd798a9
underain <123919503+underain@users.noreply.github.com>
2025-09-05 01:55:57 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["c.dns-shop.ru"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
