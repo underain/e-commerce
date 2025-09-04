a29c00f9268b146cd0b5a1c4e5881b013b4915b8
underain <123919503+underain@users.noreply.github.com>
2025-09-05 01:56:38 +0900

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Bestseller from "@/widget/main/bestseller/bestseller";
 import Brands from "@/widget/main/brands/brands";
 import Hero from "@/widget/main/hero/hero";
-
+export const dynamic = "force-dynamic";
 export default function Home() {
   return (
     <main>
